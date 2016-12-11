2165449f7b5b6ef4abb2e43187d29bf4afe52f04
David Puerto <me@davidpuerto.com>
2016-12-10 23:54:17 -0800

    hooked up the controller and the new ./api/hotels route

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,18 +3,12 @@ var router = express.Router();
 
 // Define the route, method, and then function you would like to run.
 router
-  .route('/json')
+  .route('/hotels')
   .get(function(req, res) {
     console.log('GET the json');
     res
       .status(200)
       .json( {"jsonData" : true} );
-  })
-  .post(function(req, res) {
-    console.log('POST the json route');
-    res
-      .status(200)
-      .json( {"jsonData" : "POST received"} );
   });
 
 module.exports = router;
