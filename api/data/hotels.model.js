a0f666972b4fe60df66a716622108a31a973872b
David Puerto <me@davidpuerto.com>
2016-12-19 15:57:04 -0800

    defining first mongoose schema

---
+++ api/data/hotels.model.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+var hotelSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  stars: {
+    type: Number,
+    min: 0,
+    max: 5,
+    default: 0 // can write 'default' to offset warning for reserved keyword.
+  },
+  services: [String],
+  description: String,
+  photos: [String],
+  currency: String
+});
+
+/** mongoose.model('ModelName', modelSchema, 'collection') ** If no collection
+  * specified, mongoose will automagically point a reference to the "lowercase, * pluralized" form of the model name.
+  */
+mongoose.model('Hotel', hotelSchema);
