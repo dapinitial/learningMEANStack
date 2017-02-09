d6d18da49fdbc27fabc943a27c6cbac24f4f0a69
David Puerto <me@davidpuerto.com>
2017-02-09 03:24:37 -0800

    Beginning of authentication

---
+++ api/data/users.model.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    require: true
+  },
+  name: {
+    type: String
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+mongoose.model('User', userSchema);
